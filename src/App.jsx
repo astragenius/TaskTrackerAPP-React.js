10658d1e4c431ed82e05fb223a0214e5c32b9477
astragenius <darthmalak86@gmail.com>
2023-03-09 22:27:08 +0100

    styling - toDo styling and todo container styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import AddTask from "./components/AddTask"
+import ToDo from "./components/ToDo"
 
 
 
@@ -17,13 +18,15 @@ function App() {
           <AddTask taskList={taskList} setTaskList={setTaskList}/>
           <p className="text-xl">to add a new task</p>
       </div>
-      {taskList.map((task, i) =>
-        <>
+      <section>
+        <h2 className="bg-slate-300 text-2xl font-semibold uppercase w-3/4 max-w-lg ml-6 my-4 py-2 px-4">To Do:</h2>
+        {taskList.map((task, i) =>
+          <>
+            <ToDo key={i} task={task}/>
           
-          <p>{task.projectName}</p>
-          <p>{task.projectDescription}</p>
-        </>
-      )}
+          </>
+        )}
+      </section>
     </div>
   )
   
--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 
-const ToDo = () => {
+const ToDo = ({task}) => {
   return (
-   <>
-                
-   </>
+    <>  
+        <section className='flex flex-col items-start justify-start bg-white my-4 ml-6 py-4 px-6 w-3/4 max-w-lg'>
+            <p className='text-xl font-semibold'>{task.projectName}</p>
+            <p className='text-lg py-2'>{task.projectDescription}</p>
+
+            <button>Delete</button>
+        </section>
+    </>
   )
 }
 
