d218d7e5ea8a7396729f6e2c8c6f5e5a7eb16d40
astragenius <darthmalak86@gmail.com>
2023-03-10 21:01:24 +0100

    added - ToDo component and editTask component

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -4,10 +4,22 @@ const ToDo = ({task}) => {
   return (
     <>  
         <section className='flex flex-col items-start justify-start bg-white my-4 ml-6 py-4 px-6 w-3/4 max-w-lg'>
-            <p className='text-xl font-semibold'>{task.projectName}</p>
-            <p className='text-lg py-2'>{task.projectDescription}</p>
 
-            <button>Delete</button>
+          <div className='w-full flex flex-row justify-between'>
+              <p className='text-xl font-semibold'>{task.projectName}</p>
+              <button className='bg-blue-300 text-white text-sm uppercase py-1 px-2 rounded'>Edit</button>
+
+          </div>
+         
+              <p className='text-lg py-2'>{task.projectDescription}</p>
+           
+
+          <div className='w-full flex justify-center'>
+
+              <button className='bg-red-500 text-white text-sm uppercase font-semibold py-1 px-4 rounded-lg'>Delete</button>
+          </div>
+
+
         </section>
     </>
   )
